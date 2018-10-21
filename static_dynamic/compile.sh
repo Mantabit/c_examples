bf64a6776e591583082fdb35953f555d547ea606
Mantabit <dvarx@gmx.ch>
2018-10-21 21:59:17 +0200

    added include directory

--- static_dynamic/compile.sh
+++ static_dynamic/compile.sh
@@ -1,11 +1,11 @@
 #compile the source files into static object files
-gcc -c main.c -o main.o
-gcc -c add.c -o static/add.o
-gcc -c answer.c -o static/answer.o
+gcc -c main.c -I include -o main.o
+gcc -c add.c -I include -o static/add.o
+gcc -c answer.c -I include -o static/answer.o
 
 #compile the source files into dynamic object files
-gcc -c add.c -fPIC -o dynamic/add.o
-gcc -c answer.c -fPIC -o dynamic/answer.o
+gcc -c add.c -I include -fPIC -o dynamic/add.o
+gcc -c answer.c -I include -fPIC -o dynamic/answer.o
 
 #create static library
 ar rcs static/libstatic.a static/answer.o static/add.o
@@ -14,7 +14,7 @@ ar rcs static/libstatic.a static/answer.o static/add.o
 gcc -shared dynamic/add.o dynamic/answer.o -o dynamic/libdynamic.so
 
 #link using static library
-gcc main.o -Lstatic -lstatic -o main_static.out
+gcc main.o -L static -l static -o main_static.out
 
 #link using dynamic library
 gcc main.o -Ldynamic -ldynamic -o main_dynamic.out
